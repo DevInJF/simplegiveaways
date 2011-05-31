952e23baad88dea220ac133dcd525271d5150df5
lightyrs <harris@harrisnovick.com>
2011-05-31 01:29:14 -0400

    Heroku

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,9 @@ module SGA
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    # Jammit + Heroku
+    config.gem "jammit"
   end
 end
 
