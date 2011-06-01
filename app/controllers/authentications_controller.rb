45943e78f087b79160b2921d5843dd9c3018ba2b
lightyrs <harris@harrisnovick.com>
2011-05-31 23:47:44 -0400

    Delayed Job on Heroku

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -9,7 +9,6 @@ class AuthenticationsController < ApplicationController
     if authentication
       flash[:notice] = "Signed in successfully."
       sign_in_and_redirect(:user, authentication.user)
-      Delayed::Job.enqueue(authentication.user.generate_account)
     elsif current_user
       current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
       flash[:notice] = "Authentication successful."
