228cbcad4cec05d9aa3e9bacb74e36311e5a2253
lightyrs <harris@harrisnovick.com>
2011-06-12 20:31:37 -0400

    Removing bad migration.

--- db/migrate/20110612220722_add_uniqueness_to_credit_cards_users.rb
+++
@@ -1,9 +0,0 @@
-class AddUniquenessToCreditCardsUsers < ActiveRecord::Migration
-  def self.up
-    add_index :credit_cards_users, [:credit_card_id, :user_id], :unique => true, :name => 'by_credit_card_and_user'
-  end
-
-  def self.down
-    remove_index :credit_cards_users, [:credit_card_id, :user_id]
-  end
-end
