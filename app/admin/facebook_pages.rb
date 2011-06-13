d25fabfc17ad35470ffdb7df0bcac5f616a4c53f
lightyrs <harris@harrisnovick.com>
2011-06-12 21:02:29 -0400

    Active Admin » Accounting for new associations.

--- app/admin/facebook_pages.rb
+++ app/admin/facebook_pages.rb
@@ -6,14 +6,12 @@ ActiveAdmin.register FacebookPage do
   # Filterable attributes on the index screen
   filter :name
   filter :likes
-  filter :user_id
   filter :created_at
 
   # Customize columns displayed on the index screen in the table
   index do
     column :name
     column :likes
-    column :user_id
     column :created_at
     default_actions
   end
