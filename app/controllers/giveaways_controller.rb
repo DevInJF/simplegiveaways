7d707dd315e4028a68c690b1e4c3e546bf170e9e
lightyrs <harris@harrisnovick.com>
2011-07-10 17:01:50 -0400

    Testing fb tab auth

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -75,10 +75,8 @@ class GiveawaysController < ApplicationController
   # GET /giveaways/tab.html
   # POST /giveaways/tab.html
   def tab
-    #@signed_request = Facebook::SignedRequest::parse_signed_request(params[:signed_request],
-    #"da7dc60be4b02073a6b584722896e6c9")
-    #@giveaway = FacebookPage.find_by_pid(@signed_request["page"]["id"]).giveaways.detect(&:is_live?)
-    @giveaway = Giveaway.first
+    @signed_request = Facebook::SignedRequest::parse_signed_request(params[:signed_request],"da7dc60be4b02073a6b584722896e6c9")
+    @giveaway = FacebookPage.find_by_pid(@signed_request["page"]["id"]).giveaways.detect(&:is_live?)
     render :layout => "tab"
   end
 
