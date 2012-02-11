551022751414beb8ce58deac0d8cd8f89a661206
Harris Novick <harris@harrisnovick.com>
2012-02-11 17:15:15 -0500

    Views

--- app/controllers/facebook_pages_controller.rb
+++ app/controllers/facebook_pages_controller.rb
@@ -1,8 +1,15 @@
 class FacebookPagesController < ApplicationController
 
   before_filter :authenticate_user!
+  before_filter :get_page
 
   def show
+
+  end
+
+  private
+
+  def get_page
     @page = FacebookPage.find(params[:id])
   end
 end
