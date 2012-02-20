bac671ad80b382592f8487c1a2eed8732f0ba76a
Harris Novick <harris@harrisnovick.com>
2012-02-20 16:34:20 -0500

    Merge in Dillon's styles. Some work on user/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 # -*- encoding : utf-8 -*-
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id], :include => :facebook_pages)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
   has_many :identities, :dependent => :destroy
   has_and_belongs_to_many :facebook_pages
 
+  def avatar
+    identities.find(:all, :order => "logged_in_at desc", :limit => 1).first.avatar
+  end
+
   def retrieve_pages
     graph = Koala::Facebook::API.new(identities.where("provider = ?", "facebook").first.token)
     pages = graph.get_connections("me", "accounts")
