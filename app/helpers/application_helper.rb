cb476f503d80a1066afa16ee3a546b116c26df2f
lightyrs <harris@harrisnovick.com>
2012-02-24 23:13:07 -0500

    Layout refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # -*- encoding : utf-8 -*-
 module ApplicationHelper
+
+  def page_heading
+    content_for(:page_heading) || controller.controller_name
+  end
+
+  def sidebar_content
+    content_for(:sidebar) || ""
+  end
 end
