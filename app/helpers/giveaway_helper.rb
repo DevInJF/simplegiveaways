81f471e6e4331bc3be6e86ae524ecf4ff7630460
Harris Novick <harris@harrisnovick.com>
2012-02-28 23:49:26 -0500

    Basic giveaways/show view.

--- app/helpers/giveaway_helper.rb
+++ app/helpers/giveaway_helper.rb
@@ -13,4 +13,11 @@ module GiveawayHelper
       ""
     end
   end
+
+  def giveaway_terms(terms)
+    terms = terms.values.reject(&:blank?).first
+    if terms.match(/^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix).present?
+      "<a href='#{terms}'>#{terms}</a>".html_safe
+    end
+  end
 end
