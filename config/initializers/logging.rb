6b76f6ad13efe4dc8198c769b87ddf424af4c39a
Harris Novick <harris@harrisnovick.com>
2012-08-06 04:28:15 -0400

    Remove logging gem from production

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,44 +1,47 @@
-require 'logging'
+unless Rails.env = "production"
 
-Logging.color_scheme('bright',
-  levels: {
-    info: :green,
-    warn: :yellow,
-    error: :red,
-    fatal: [:white, :on_red]
-  },
-  date: :blue,
-  logger: :cyan,
-  message: :white
-)
+  require 'logging'
 
-Logging.appenders.stdout(
-  'stdout',
-  layout: Logging.layouts.pattern(
-    pattern: '[%d] %-5l %c: %m\n',
-    color_scheme: 'bright'
+  Logging.color_scheme('bright',
+    levels: {
+      info: :green,
+      warn: :yellow,
+      error: :red,
+      fatal: [:white, :on_red]
+    },
+    date: :blue,
+    logger: :cyan,
+    message: :white
   )
-)
 
-Logging.appenders.file(
-  'log/development/debug.log',
-  layout: Logging.layouts.pattern(
-    pattern: '[%d] %-5l %c: %m\n',
-    color_scheme: 'bright'
-  )
-)
-
-Logging.appenders.file(
-  'log/development.log',
-  layout: Logging.layouts.pattern(
+  Logging.appenders.stdout(
+    'stdout',
+    layout: Logging.layouts.pattern(
       pattern: '[%d] %-5l %c: %m\n',
       color_scheme: 'bright'
+    )
   )
-)
 
-$log = Logging.logger['SGA']
-$log.add_appenders(
-    Logging.appenders.stdout,
-    Logging.appenders.file("#{Rails.root}/log/development/debug.log"),
-    Logging.appenders.file("#{Rails.root}/log/development.log")
-)
+  Logging.appenders.file(
+    'log/development/debug.log',
+    layout: Logging.layouts.pattern(
+      pattern: '[%d] %-5l %c: %m\n',
+      color_scheme: 'bright'
+    )
+  )
+
+  Logging.appenders.file(
+    'log/development.log',
+    layout: Logging.layouts.pattern(
+        pattern: '[%d] %-5l %c: %m\n',
+        color_scheme: 'bright'
+    )
+  )
+
+  $log = Logging.logger['SGA']
+  $log.add_appenders(
+      Logging.appenders.stdout,
+      Logging.appenders.file("#{Rails.root}/log/development/debug.log"),
+      Logging.appenders.file("#{Rails.root}/log/development.log")
+  )
+end
