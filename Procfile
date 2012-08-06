e7c711f944cf1661bc8bb191888be4177db88077
Harris Novick <harris@harrisnovick.com>
2012-08-06 08:11:53 -0400

    heroku

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 web: bundle exec thin start -p $PORT -e $RACK_ENV
-worker: bundle exec sidekiq
+worker: bundle exec sidekiq -e $RACK_ENV
 redis: redis-server
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+# require 'open-uri'
+
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+# REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
