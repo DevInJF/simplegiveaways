15a9e6a7c2b624780e20eb350b9182d9aead3948
lightyrs <harris@lightyrs.com>
2012-08-12 19:10:02 -0400

    s3

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,8 +1,1 @@
-# -*- encoding : utf-8 -*-
-if Rails.env == "production"
-  # set credentials from ENV hash
-  S3_CREDENTIALS = {access_key_id: ENV['S3_KEY'], secret_access_key: ENV['S3_SECRET'], bucket: "sga-production"}
-else
-  # get credentials from YML file
-  S3_CREDENTIALS = Rails.root.join("config/s3.yml")
-end
+S3_CREDENTIALS = Rails.root.join("config/s3.yml")
