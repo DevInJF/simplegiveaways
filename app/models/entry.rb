b4072583abf51aa9a587f8d671e2a107825b06be
Harris Novick <harris@harrisnovick.com>
2012-08-13 02:37:30 -0400

    is_viral accessor

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,7 +3,7 @@ class Entry < ActiveRecord::Base
 
   attr_accessible :email, :has_liked, :name, :fb_url, :datetime_entered,
                   :wall_post_count, :request_count, :convert_count,
-                  :status, :uid, :ref_ids, :referrer_id
+                  :status, :uid, :ref_ids, :referrer_id, :is_viral
 
   has_many :audits, as: :auditable
 
