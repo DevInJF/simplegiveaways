cff7c9df566738ff186d880a2a25ddf1aef75228
Harris Novick <harris@harrisnovick.com>
2012-08-15 01:12:18 -0400

    Add jetbrains config (.idea/**/*) to .gitignore

--- app/controllers/giveaways_controller.rb
+++ app/controllers/giveaways_controller.rb
@@ -34,7 +34,7 @@ class GiveawaysController < ApplicationController
     @giveaway = Giveaway.find_by_id(params[:id])
     @page = @giveaway.facebook_page
     @flot = { page_likes: Graph.new(@page).page_likes,
-              net_likes: Graph.new(@page).net_likes,
+              net_likes: Graph.new(@giveaway).net_likes,
               entries: Graph.new(@giveaway).entries,
               views: Graph.new(@giveaway).views }
   end
