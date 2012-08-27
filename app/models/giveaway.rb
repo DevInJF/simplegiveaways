1fc06ecbbd73091c4db073309eed0f113bf773a4
Harris Novick <harris@harrisnovick.com>
2012-08-27 01:24:01 -0400

    tab position

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -210,7 +210,8 @@ class Giveaway < ActiveRecord::Base
     Rails.logger.debug(facebook_page.pid.inspect.white_on_green)
     graph_client.put_object( facebook_page.pid, "tabs", tab: "app_#{FB_APP_ID}",
                                                         custom_name: custom_fb_tab_name,
-                                                        custom_image_url: feed_image(:thumb))
+                                                        custom_image_url: feed_image(:thumb),
+                                                        position: 0)
   end
 
   def delete_tab
