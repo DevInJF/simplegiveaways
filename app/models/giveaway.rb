aa7b9e80060eb616591770b590941ab105b31b1f
Harris Novick <harris@harrisnovick.com>
2012-09-03 17:30:44 -0400

    Make giveaways show up as ended in app even before they are removed from fb (30 sec delay)

--- app/models/giveaway.rb
+++ app/models/giveaway.rb
@@ -21,7 +21,7 @@ class Giveaway < ActiveRecord::Base
   has_many :likes
 
   scope :active, lambda {
-    where("start_date IS NOT NULL AND end_date IS NOT NULL AND start_date <= ? AND end_date >= ?", Time.zone.now, Time.zone.now).limit(1)
+    where("active IS TRUE")
   }
   scope :pending, lambda {
     where("start_date >= ? AND end_date >= ? OR start_date IS NULL OR end_date IS NULL", Time.zone.now, Time.zone.now)
