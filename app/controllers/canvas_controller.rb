9f07a66d86c537ca410b28cc55e6aeaecfb4e0d6
Harris Novick <harris@harrisnovick.com>
2012-09-06 23:06:09 -0400

    Fix requests bug

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -9,22 +9,35 @@ class CanvasController < ApplicationController
   def index
     if params["request_ids"]
 
-      request_ids = [params["request_ids"].split("%2C").split(",")].compact.flatten
+      @request_ids = [params["request_ids"].split("%2C").split(",")].compact.flatten
 
       @oauth = Koala::Facebook::OAuth.new(FB_APP_ID, FB_APP_SECRET)
+      @graph = Koala::Facebook::API.new(@oauth.get_app_access_token)
 
-      graph = Koala::Facebook::API.new(@oauth.get_app_access_token)
-      request = graph.get_object(request_ids.last)
+      if @request = select_request
 
-      @giveaway = Giveaway.select("id, title, giveaway_url").find_by_id(JSON.parse(request["data"])["giveaway_id"])
-      @app_data = "ref_#{JSON.parse(request['data'])['referrer_id']}"
+        @giveaway = Giveaway.select("id, title, giveaway_url").find_by_id(JSON.parse(@request["data"])["giveaway_id"])
+        @app_data = "ref_#{JSON.parse(@request['data'])['referrer_id']}"
 
-      FbAppRequestWorker.perform_async(request_ids.last, params[:signed_request])
+        FbAppRequestWorker.perform_async(@request_ids.last, params[:signed_request])
 
-      render "giveaways/apprequest", layout: false
-      ga_event("Canvas", "Canvas#index", @giveaway.title, JSON.parse(request['data'])['referrer_id'].to_i)
+        render "giveaways/apprequest", layout: false
+        ga_event("Canvas", "Canvas#index", @giveaway.title, JSON.parse(@request['data'])['referrer_id'].to_i)
+      else
+        redirect_to "http://facebook.com"
+      end
     else
-      redirect_to "http://simplegiveawayapp.com"
+      redirect_to "http://facebook.com"
     end
+  end
+
+  private
+
+  def select_request
+    graph.get_object(request_ids.pop)
+  rescue Koala::Facebook::APIError
+    graph.get_object(request_ids.pop)
+  rescue Koala::Facebook::APIError
+    graph.get_object(request_ids.pop)
   end
 end
