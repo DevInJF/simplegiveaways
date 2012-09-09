66b03ef06206d6a5d562e7f768cb4ac2b90a4d7c
Harris Novick <harris@harrisnovick.com>
2012-09-08 22:33:33 -0400

    Log stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,12 @@ class UsersController < ApplicationController
   end
 
   def deauth
+    Rails.logger.debug(params.inspect.yellow)
+    Rails.logger.debug(@signed_request.inspect.magenta)
     if @user = User.find_by_id(@signed_request["user_id"])
       # TODO: Send email (should we destroy your User account and unsubscribe?)
       @user.update_attributes(name: 'DEAUTHED_FACEBOOK')
-      @user.identities.where(:provider => 'facebook').each(&:destroy)
+      @user.identities.where(:provider => params[:provider]).each(&:destroy)
     end
     head :ok
   end
