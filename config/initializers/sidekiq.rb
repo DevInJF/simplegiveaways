34308bdf84a3969a986e88dd1c63bd686838fb88
Harris Novick <harris@harrisnovick.com>
2012-09-27 00:33:06 -0400

    Better sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :size => (Sidekiq.options[:concurrency] + 2) }
+  config.redis = { :url => ENV["REDISTOGO_URL"], :size => (Sidekiq.options[:concurrency] + 2) }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { :url => ENV["REDISTOGO_URL"], :size => 1 }
 end
