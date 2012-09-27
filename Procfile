6c06c075737bfecbadcac30e54dce526e7205bdd
Harris Novick <harris@harrisnovick.com>
2012-09-27 00:15:48 -0400

    Better sidekiq

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec thin -p $PORT -e $RACK_ENV -R config.ru start
+web: bundle exec rails server thin start -p $PORT -e $RACK_ENV
 worker: bundle exec sidekiq -e $RACK_ENV
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,3 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV["REDISTOGO_URL"] }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { :url => ENV["REDISTOGO_URL"], :size => 1 }
+  config.redis = { :size => (Sidekiq.options[:concurrency] + 2) }
 end
