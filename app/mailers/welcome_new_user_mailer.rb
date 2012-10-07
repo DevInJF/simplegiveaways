6f92c9c5aa43f040465e67ae468235faf5f8964f
Harris Novick <harris@harrisnovick.com>
2012-10-07 17:24:52 -0400

    Try to get these mailers working.

--- app/mailers/welcome_new_user_mailer.rb
+++ app/mailers/welcome_new_user_mailer.rb
@@ -5,6 +5,8 @@ class WelcomeNewUserMailer < ActionMailer::Base
     mail subject: 'Welcome to Simple Giveaways',
          to: @identity.email,
          from: 'support@simplegiveaways.com',
+         sent_on: Time.zone.now,
+         body: { user_name: @identity.user.name }
          template_path: 'mailers'
   end
 end
