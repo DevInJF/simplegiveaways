7f6927cdfc10e420218fea251c1e7024782891c6
Harris Novick <harris@harrisnovick.com>
2012-10-08 00:21:26 -0400

    End giveaway mailer

--- app/mailers/giveaway_notice_mailer.rb
+++ app/mailers/giveaway_notice_mailer.rb
@@ -6,8 +6,9 @@ class GiveawayNoticeMailer < ActionMailer::Base
   def start(user, giveaway)
     @giveaway = giveaway
     @user_first_name = user.name.split(" ")[0] rescue user.name
+    email_with_name = "#{@user_first_name} <#{user.identities.pop.email.email}>"
     mail subject: 'Your Giveaway Has Begun',
-         to: user.identities.pop.email,
+         to: email_with_name,
          from: 'support@simplegiveaways.com',
          template_path: 'mailers/giveaway_notice_mailer',
          template_name: 'start'
@@ -16,8 +17,9 @@ class GiveawayNoticeMailer < ActionMailer::Base
   def end(user, giveaway)
     @giveaway = giveaway
     @user_first_name = user.name.split(" ")[0] rescue user.name
+    email_with_name = "#{@user_first_name} <#{user.identities.pop.email.email}>"
     mail subject: 'Your Giveaway Has Ended',
-         to: user.identities.pop.email,
+         to: email_with_name,
          from: 'support@simplegiveaways.com',
          template_path: 'mailers/giveaway_notice_mailer',
          template_name: 'end'
