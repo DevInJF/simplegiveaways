b25d5e16bbfd67eed481477927a6f23b7fd9bdfa
Harris Novick <harris@harrisnovick.com>
2013-11-03 23:35:48 -0500

    wip: facebook page giveaways view

--- app/controllers/facebook_pages_controller.rb
+++ app/controllers/facebook_pages_controller.rb
@@ -4,7 +4,6 @@ class FacebookPagesController < ApplicationController
   load_and_authorize_resource through: :current_user
 
   def show
-    @page = FacebookPage.find(params[:id])
-    redirect_to active_facebook_page_giveaways_path(@page)
+    @page = FacebookPage.find_by_id(params[:id])
   end
 end
