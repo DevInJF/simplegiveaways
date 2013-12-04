2347fd7f66dc8893d4114eef40403c1e1de757d7
Harris Novick <harris@harrisnovick.com>
2013-12-03 21:26:14 -0500

    remove pages from user for which he is no longer an admin

--- app/models/facebook_page.rb
+++ app/models/facebook_page.rb
@@ -75,47 +75,6 @@ class FacebookPage < ActiveRecord::Base
     subscription.subscription_plan.name rescue nil
   end
 
-  def self.retrieve_fb_meta(user, pages, csrf_token)
-    pages = FacebookPage.select_pages(pages).compact.flatten
-    page_count = (pages.size - 1)
-
-    pages.each_with_index do |page_hash, index|
-                  page = page_hash[:page]
-               fb_meta = page_hash[:fb_meta][:data]
-      fb_avatar_square = page_hash[:fb_meta][:avatar_square]
-       fb_avatar_large = page_hash[:fb_meta][:avatar_large]
-
-      @page = FacebookPage.find_or_create_by_pid(page["id"])
-
-      previous_likes = @page.likes || fb_meta["likes"]
-
-      @page.update_attributes(
-        name: page["name"],
-        category: page["category"],
-        pid: page["id"],
-        token: page["access_token"],
-        avatar_square: fb_avatar_square,
-        avatar_large: fb_avatar_large,
-        description: fb_meta["description"],
-        url: fb_meta["link"],
-        likes: fb_meta["likes"],
-        has_added_app: fb_meta["has_added_app"]
-      )
-
-      jug_data = { markup: @page.preview_template(previous_likes),
-                   menu_item: @page.menu_item_template,
-                   is_last: "#{index == page_count}" }
-
-      Juggernaut.url = ENV["REDISTOGO_URL"]
-      Juggernaut.publish("users#show_#{csrf_token}", jug_data.to_json)
-
-      unless user.facebook_pages.include? @page
-        @page.refresh_likes
-        user.facebook_pages << @page
-      end
-    end
-  end
-
   def refresh_likes
     batch = FacebookPage.graph_data(self)
 
@@ -135,45 +94,6 @@ class FacebookPage < ActiveRecord::Base
     users.map do |user|
       user.identities.first.email if user
     end
-  end
-
-  def self.select_pages(pages)
-    pages = pages.reject do |page|
-      page["category"] == "Application"
-    end
-
-    pages.collect do |page|
-      if FacebookPage.page_eligible?(batch = FacebookPage.graph_data(page))
-        { page: page, fb_meta: batch }
-      end
-    end
-  end
-
-  def self.graph_data(page)
-    batch = FacebookPage.batch_data(page)
-
-    fb_meta          = batch[0]
-    fb_avatar_square = batch[1]
-    fb_avatar_large  = batch[2]
-
-    { data: fb_meta,
-      avatar_square: fb_avatar_square,
-      avatar_large: fb_avatar_large }
-  end
-
-  def self.batch_data(page)
-    @token = page["access_token"] || page.token
-    @graph = Koala::Facebook::API.new(@token)
-
-    @graph.batch do |batch_api|
-      batch_api.get_object("me")
-      batch_api.get_picture("me", type: "square")
-      batch_api.get_picture("me", type: "large")
-    end
-  end
-
-  def self.page_eligible?(batch)
-    batch[:data]["link"].include? "facebook.com"
   end
 
   def menu_item_template
@@ -209,5 +129,91 @@ class FacebookPage < ActiveRecord::Base
 
   def path
     Rails.application.routes.url_helpers.facebook_page_path(self)
+  end
+
+  class << self
+
+    def select_pages(pages)
+      pages = pages.reject do |page|
+        page["category"] == "Application"
+      end
+
+      pages.collect do |page|
+        if page_eligible?(batch = graph_data(page))
+          { page: page, fb_meta: batch }
+        end
+      end
+    end
+
+    def graph_data(page)
+      batch = batch_data(page)
+
+      fb_meta          = batch[0]
+      fb_avatar_square = batch[1]
+      fb_avatar_large  = batch[2]
+
+      { data: fb_meta,
+        avatar_square: fb_avatar_square,
+        avatar_large: fb_avatar_large }
+    end
+
+    def batch_data(page)
+      @token = page["access_token"] || page.token
+      @graph = Koala::Facebook::API.new(@token)
+
+      @graph.batch do |batch_api|
+        batch_api.get_object("me")
+        batch_api.get_picture("me", type: "square")
+        batch_api.get_picture("me", type: "large")
+      end
+    end
+
+    def page_eligible?(batch)
+      batch[:data]["link"].include? "facebook.com"
+    end
+
+    def retrieve_fb_meta(user, pages, csrf_token)
+      pages = select_pages(pages).compact.flatten
+      page_count = (pages.size - 1)
+
+      pages.each_with_index do |page_hash, index|
+                    page = page_hash[:page]
+                 fb_meta = page_hash[:fb_meta][:data]
+        fb_avatar_square = page_hash[:fb_meta][:avatar_square]
+         fb_avatar_large = page_hash[:fb_meta][:avatar_large]
+
+        @page = find_or_create_by_pid(page["id"])
+
+        previous_likes = @page.likes || fb_meta["likes"]
+
+        @page.update_attributes(
+          name: page["name"],
+          category: page["category"],
+          pid: page["id"],
+          token: page["access_token"],
+          avatar_square: fb_avatar_square,
+          avatar_large: fb_avatar_large,
+          description: fb_meta["description"],
+          url: fb_meta["link"],
+          likes: fb_meta["likes"],
+          has_added_app: fb_meta["has_added_app"]
+        )
+
+        unless user.facebook_pages.include? @page
+          @page.refresh_likes
+          user.facebook_pages << @page
+        end
+      end
+
+      remove_outdated_pages(user, pages)
+    end
+
+    def remove_outdated_pages(user, pages)
+      user_pids = user.facebook_pages.map(&:pid)
+      fb_pids = pages.map { |page_hash| page_hash[:page]["id"] }
+      (user_pids - fb_pids).each do |pid|
+        user.facebook_pages.delete(find_by_pid(pid))
+      end
+    end
   end
 end
