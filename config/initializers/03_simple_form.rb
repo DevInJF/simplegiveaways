524acc34b241f4334a759edbd51df17a8a1c99bd
Harris Novick <harris@harrisnovick.com>
2014-02-04 00:16:17 -0500

    finally fix simple_form activeadmin incompatibility

--- config/initializers/03_simple_form.rb
+++ config/initializers/03_simple_form.rb
@@ -12,16 +12,25 @@ SimpleForm.setup do |config|
     TextInput
   ]
 
+  # Instead of creating top-level custom input classes like TextInput, we wrap it into a module and override
+  # mapping in SimpleForm::FormBuilder directly
+  #
+  SimpleFormBootstrapInputs = Module.new
   inputs.each do |input_type|
     superclass = "SimpleForm::Inputs::#{input_type}".constantize
 
-    new_class = Class.new(superclass) do
+    new_class = SimpleFormBootstrapInputs.const_set(input_type, Class.new(superclass) do
       def input_html_classes
         super.push('form-control')
       end
-    end
+    end)
 
-    Object.const_set(input_type, new_class)
+    # Now override existing usages of superclass with new_class
+    SimpleForm::FormBuilder.mappings.each do |(type, target_class)|
+      if target_class == superclass
+        SimpleForm::FormBuilder.map_type(type, to: new_class)
+      end
+    end
   end
 
   config.boolean_style = :nested
